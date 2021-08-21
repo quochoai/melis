a15258078387b9f6c5bf610af485de7c22915cbc
Quốc Hoài <quochoai.2202@gmail.com>
2021-08-21 16:50:29 +0700

    add file to view products

---
+++ admin/views/products/data.php
@@ -0,0 +1,56 @@
+<?php
+    include("../../../require_inc.php");
+    
+    $options = array(
+        'draw' => $_REQUEST['draw'],
+        'search_value' => $_REQUEST['search'],
+        'limit' => $_REQUEST['length'],
+        'offset' => $_REQUEST['start'],
+        'order' => 0,
+        'dir' => $_REQUEST['order.0.dir'],
+    );
+    $search = $_REQUEST["search"];
+
+    if ($search['value'] != '')
+        $w = "and (name_vi like '%".$search['value']."%' or name_en like '%".$search['value']."%')";
+    else
+        $w = "";
+    $catess = $h->getAll("id, name_vi, name_en, active", "products", "cm = 1 and deleted_at is null $w", "id asc");
+    if (count($catess) > 0) {
+        $totalData = count($catess);
+        $totalFiltered = $totalData;
+        $cates = $h->getAll("id, name_vi, name_en, active", "products", "cm = 1 and deleted_at is null $w", "id asc limit ".$options['offset'].", ".$options['limit']);
+        foreach ($cates as $kc => $cate) {
+            $no = $kc + 1;
+            if ($cate['active'] == 1) {
+                $fontawesome = 'eye';
+                $tte = $lang['hidden'];
+            } else {
+                $fontawesome = 'eye-slash';
+                $tte = $lang['active'];
+            }
+            $a[] = array(
+                "DT_RowId" => 'cate'.$cate['id'],
+                //"DT_RowClass" => "category",
+                "no" => $no,
+                "id" => $cate['id'],  
+                "name_vi" => $cate['name_vi'], 
+                "name_en" => $cate['name_en'], 
+                "manage_product" => "<a href='".$def['link_product']."/".$cate['id']."' title='".$lang['manage_product']."'>".$lang['manage']."</a>", 
+                "actions" => "<a data-id='".$cate['id']."' rel='".$cate['active']."' class='btn btn-success btn-sm active' id='ht".$cate['id']."' title='".$tte."'><i id='hs".$cate['id']."' class='fas fa-".$fontawesome."'></i></a> | <a href='javascript:void(0)' rel='".$cate['id']."' class='btn btn-success btn-sm update' title='".$lang['update']."'><i class='fas fa-edit'></i></a> | <a href='javascript:void(0)' rel='".$cate['id']."' class='btn btn-danger btn-sm delete' title='".$lang['delete']."'><i class='fas fa-trash'></i></a>",
+                "search_value" => $search_value
+            );
+        }
+    } else {
+        $totalFiltered = 0;
+        $a = array();
+    }
+    
+    $json_data = array(
+        "draw"            => $options['draw'],
+        "recordsTotal"    => $totalData,
+        "recordsFiltered" => $totalFiltered,
+        "data"            => $a
+    );
+    echo json_encode($json_data);
+    
---
+++ admin/views/products/list.php
@@ -0,0 +1,155 @@
+<!-- Main content -->
+<section class="content">
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-12">
+        <div class="card">
+          <div class="card-header container-fluid">
+            <div class="row">
+                <h3 class="col-md-10 card-title">
+                    <?php echo $lang['manage_product'] ?>
+                </h3>
+                <div class="col-md-2"><a class="float-right btn btn-success" data-toggle="modal" data-target="#modal-add-product"><i class="fas fa-plus"></i><?php echo $lang['addnew'] ?></a></div>
+            </div>
+          </div>
+          <!-- /.card-header -->
+          <div class="card-body">
+            <table id="products" class="table table-bordered table-hover">
+              <thead>
+              <tr>
+                <th width="7%" class="text-center"><?php echo $lang['no.'] ?></th>
+                <th width="32%" class="text-center"><?php echo $lang['name_product'].' (Vie)' ?></th>
+                <th width="32%" class="text-center"><?php echo $lang['name_product'].' (Eng)' ?></th>
+                <th width="14%" class="text-center"><?php echo $lang['sort'] ?></th>
+                <th width="15%" class="text-center"><?php echo $lang['actions'] ?></th>
+              </tr>
+              </thead>
+            </table>
+          </div>
+          <!-- /.card-body -->
+        </div>
+        <!-- /.card -->
+      </div>
+      <!-- /.col -->
+    </div>
+    <!-- /.row -->
+  </div>
+  <!-- /.container-fluid -->
+</section>
+<!-- /.content -->
+<!-- add -->
+<div class="modal fade" id="modal-add-product">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+            <div class="modal-header bg-success">
+                <h5 class="modal-title text-uppercase"><?php echo $lang['add_product'] ?></h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true" class="text-white">&times;</span>
+                </button>
+            </div>
+            <form method="post" action="<?php echo $def['link_process_add_product'] ?>" id="form_add" enctype="multipart/form-data">
+                <div class="modal-body container-fluid">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label class="col-form-label" for="name"><?php echo $lang['name_product'].' (Vie)' ?></label>
+                                <input type="text" class="form-control" name="data[name_vi]" id="name_vi" />
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label class="col-form-label" for="name"><?php echo $lang['name_product'].' (Eng)' ?></label>
+                                <input type="text" class="form-control" name="data[name_en]" id="name_en" />
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label class="col-form-label" for="name"><?php echo $lang['title_website'].' (Vie)' ?></label>
+                                <input type="text" class="form-control" name="data[title_vi]" id="title_vi" />
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label class="col-form-label" for="name"><?php echo $lang['title_website'].' (Eng)' ?></label>
+                                <input type="text" class="form-control" name="data[title_en]" id="title_en" />
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label class="col-form-label" for="name"><?php echo $lang['description'].' (Vie)' ?></label>
+                                <textarea class="form-control" name="data[desc_vi]" id="desc_vi" rows="4"></textarea>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label class="col-form-label" for="name"><?php echo $lang['description'].' (Eng)' ?></label>
+                                <textarea class="form-control" name="data[desc_en]" id="desc_en" rows="4"></textarea>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label class="col-form-label" for="name"><?php echo $lang['keyword'].' (Vie)' ?></label>
+                                <textarea class="form-control" name="data[keyw_vi]" id="keyw_vi" rows="4"></textarea>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label class="col-form-label" for="name"><?php echo $lang['keyword'].' (Eng)' ?></label>
+                                <textarea class="form-control" name="data[keyw_en]" id="keyw_en" rows="4"></textarea>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="submit" id="add" class="btn btn-success"><?php echo $lang['save'] ?> <i class="fas fa-save"></i></button><button type="reset" class="btn btn-info"><?php echo $lang['reset'] ?> <i class="fas fa-undo"></i></button>
+                </div>
+            </form>
+        </div>
+        <!-- /.modal-content -->
+    </div>
+    <!-- /.modal-dialog -->
+</div>
+<!-- update -->
+<div class="modal fade" id="modal-update-product">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+            <div class="modal-header bg-success">
+                <h5 class="modal-title text-uppercase"><?php echo $lang['update_product'] ?></h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true" class="text-white">&times;</span>
+                </button>
+            </div>
+            <form method="post" action="<?php echo $def['link_process_update_product'] ?>" id="form_update" enctype="multipart/form-data">
+                <div class="modal-body container-fluid" id="product_update"></div>
+                <div class="modal-footer">
+                    <button type="submit" id="update" class="btn btn-success"><?php echo $lang['update'] ?> <i class="fas fa-edit"></i></button>
+                </div>
+            </form>
+        </div>
+        <!-- /.modal-content -->
+    </div>
+    <!-- /.modal-dialog -->
+</div>
+<script type="text/javascript">
+    var lang_url = "<?php echo $def['theme'].'plugins/datatables/vn.json' ?>";
+    var column_not_sort = "3, 4";
+    var backend_products_list = "views/products/data.php";
+    var table_id = "#products";
+    var all_page = "<?php echo $lang['all'] ?>";
+    var link_delete = "<?php echo $def['link_process_delete_product'] ?>";
+    var link_active_product = "<?php echo $def['link_active_product'] ?>";
+    var link_get_product = "<?php echo $def['link_get_product'] ?>";
+    var session_timeout = '<?php echo $lang['session_timeout'] ?>';
+    var system_error = '<?php echo $lang['system_error'] ?>';
+    var not_name_product_vi = "<?php echo $lang['not_name_product_vi'] ?>";
+    var not_name_product_en = "<?php echo $lang['not_name_product_en'] ?>";
+    var processing = "<?php echo $lang['processing'] ?>";
+    var text_save = "<?php echo $lang['save'] ?>";
+    var add_success = '<?php echo $lang['add_success'] ?>';
+    var text_update = "<?php echo $lang['update'] ?>";
+    var update_success = '<?php echo $lang['update_success'] ?>';
+    var conf = "<?php echo $lang['confirm_delete'] ?>";
+    var hidden = "<?php echo $lang['hidden'] ?>";
+    var shows = "<?php echo $lang['active'] ?>";
+</script>
+<script src="views/products/data.js"></script>
